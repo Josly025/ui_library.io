da39a434f660d78e4d698deab82b84b2c5468898
Grantjos12 <grantjoslyn@gmail.com>
2021-01-05 22:07:44 -0600

    new hamburger button and fixed icons

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("this is app.js");
+
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
